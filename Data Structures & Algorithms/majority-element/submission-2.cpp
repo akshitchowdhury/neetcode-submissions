20961c5e4180120ad041d86483e5c12fdbd6bf72
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2026-08-29 21:48:32 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+  int res = -1;
+  map<int,int> m;
+    
+  for(int n : nums){
+
+    m[n]++;
+    
+  }
+
+    for (auto &[key, val] : m){ cout << key << ":" << val << " ";
+      int temp = res;
+      res = max(key,temp);
+    }
+    return res;
+    }
+};
